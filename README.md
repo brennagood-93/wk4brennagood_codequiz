6065065d7df07ddf1809040d188039be72df7ffa
Brenna Good <brenna.elise.good@gmail.com>
2020-06-24 22:00:12 -0700

    First stage of homework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wk4brennagood_codequiz
+
+Create a quiz
+A quiz about what? = Wine! 
+First create a start button
---
+++ script.js
@@ -0,0 +1,26 @@
+//change display type for div holding content, start timer
+//function changing the question / answer text to the next question when each question is answered
+//event listenter that pertains to all answer buttons that runs funcion a (changes to next question)
+    //by changing the textContent for the question space and ansewr content
+//last question changes class of master divs to hide question space and show results screen
+//function keeping score
+//runs if correct answer is clicked; 
+    //if statements to add to a scoce variable as the test continues
+//when last answer is clicked 
+//when user submits initials, run a funtion that adds the score to the local storage
+//get the data from local storage to show the scores in a list
+Collapse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
